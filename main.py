858a92b08cdfa22a1e32fd4c508d7847b09c50af
PeterMaximov <cambo555@yandex.ru>
2023-04-10 17:54:38 +0300

    commit 2

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dogs.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+dogs_db = SQLAlchemy(app)
+
+
+#class Dogs_Search(dogs_db.Model):
+  #  pass
 
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template("index.html")
 
 
 @app.route('/help')
@@ -22,6 +33,11 @@ def about():
 @app.route('/test')
 def test():
     return render_template("test.html")
+
+
+@app.route('/result_search.html')
+def result_search():
+    return render_template("result_search.html")
 
 
 if __name__ == '__main__':
